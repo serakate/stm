f30df0bf214f0250c5326afc272b06e225b0dc12
serakate <serakate@yandex.ru>
2023-03-05 20:33:11 +0300

    table page, menu

--- stm/__init__.py
+++ stm/__init__.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/index")
 def index():
-    return "index"
+    return render_template("index.html", title="Старт")
 
 
-@app.route("/about")
-def about():
-    return "<h1>О сайте</h1>"
+@app.route("/table")
+def get_table():
+    return render_template(
+        "table.html",
+        title="Таблица данных",
+        table=[
+            ["ID", "Sensor", "Location", "Value", "Timestamp"],
+            [1, "AN", 2, 3, "03.03.2023"],
+            [2, "AN", 2, 10, "05.03.2023"],
+        ],
+    )
 
 
 if __name__ == "__main__":
